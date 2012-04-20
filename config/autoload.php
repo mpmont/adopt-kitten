122124daf8894649fa62e115849a1602e1890a45
Marco Monteiro <mpmont@gmail.com>
2012-04-20 19:59:02 +0100

    forgot this one.

--- config/autoload.php
+++ config/autoload.php
@@ -1,8 +1,10 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * @author 		Edmundas Kondrašovas <as@edmundask.lt>
- * @license		http://www.opensource.org/licenses/MIT
+ *
+ * @original_author     Edmundas Kondrašovas <as@edmundask.lt>
+ * @author              Marco Monteiro <marco@marcomonteiro.net>
+ * @license		        http://www.opensource.org/licenses/MIT
  */
 
-$autoload['helper'] = array('placeholder');
+$autoload['helper'] = array('placekitten');
